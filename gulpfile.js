2a2389524350b0bd25b6f1f9dfbda00858d08e08
Aaron Enders <mail@aaron-enders.de>
2018-05-19 10:21:21 +0200

    Optimized output

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
-var gulp = require('gulp'),
-resources = require('gulp-resources');
+var gulp = require('gulp');
+var resources = require('gulp-resources');
 var Crawler = require('simplecrawler');
 var clean = require('gulp-clean');
-newfile           = require('gulp-file');
-timestamp           = require('time-stamp');
-prompt           = require('gulp-prompt');
-
-fs = require('fs');
-
-
+var newfile           = require('gulp-file');
+var timestamp           = require('time-stamp');
+var prompt           = require('gulp-prompt');
+var fs = require('fs');
 var lastUrl = "";
 gulp.task('resources', function(cb) {
       var argv = require('yargs').argv;
       var url= argv.url;
-
       var logfile = "./"+encodeURIComponent(url.replace(/(^\w+:|^)\/\//, ''))+"-"+timestamp("YYYY-MM-DD-mm-ss")+".csv";
       fs.writeFile(logfile, '', cb);
       fs.appendFile(logfile, "HOST;URL;TYPE;REF LASTURL;REF\r\n", function (err) { if (err) throw err; });
@@ -32,11 +28,10 @@ gulp.task('resources', function(cb) {
       });
       crawler.on("fetchcomplete",function(queueItem, responseBuffer, response) {
         if (queueItem.stateData.contentType){
-          var type = queueItem.stateData.contentType.replace("; charset=utf-8", "").replace("charset=UTF-8", "");
+          var type = queueItem.stateData.contentType.split(';')[0];
         }else{
           var type = "(undefined)";
         }
-        var type = queueItem.stateData.contentType.replace("; charset=utf-8", "");
         if (queueItem.url.indexOf(url) !== -1){
           console.log('\x1b[36m%s\x1b[0m', "Internal: ", queueItem.url);
         }else{
@@ -44,18 +39,14 @@ gulp.task('resources', function(cb) {
           fs.readFile(logfile, function (err, data) {
             if (err) throw err;
             if(data.indexOf(queueItem.url) >= 0){
-              console.log('\x1b[32m', "External, already found: ", queueItem.url+" Type: ", type);
+              console.log('\x1b[33m', "External: ", "("+type+") "+queueItem.url);
             }else{
-              console.log('\x1b[32m', "External: ", queueItem.url+" Type: ", type);
-              // if (type == "text/html"){
-              //   // console.log(queueItem);
-              // }
-              // fs.appendFile(logfile, queueItem.host+";"+queueItem.url+";"+type+";"+lastUrl+";"+queueItem.referrer+";"+"\r\n", function (err) {
+              console.log('\x1b[32m', "External: ", "("+type+") "+queueItem.url);
               var filetype= "unbekannt";
               if (type == "text/html"){
                 // console.log(queueItem);
               }
-              fs.appendFile(logfile, queueItem.host+";"+queueItem.url+";"+type+";"+lastUrl+queueItem.referrer+";"+"\r\n", function (err) {
+              fs.appendFile(logfile, queueItem.host+";"+queueItem.url+";"+type+";"+lastUrl+";"+queueItem.referrer+""+"\r\n", function (err) {
                   if (err) throw err;
               });
             }
@@ -63,6 +54,7 @@ gulp.task('resources', function(cb) {
         }
         if(queueItem.url.indexOf("charset=utf-8") >= 0
         || queueItem.url.indexOf("encoding=utf-8") >= 0
+        || queueItem.url.indexOf("charset=UTF-8") >= 0
         || queueItem.url.indexOf("charset=UTF-8") >= 0){
         }else{
           lastUrl = queueItem.url;
@@ -70,7 +62,6 @@ gulp.task('resources', function(cb) {
       });
       crawler.on("complete", function(queueItem, responseBuffer, response) {
          console.log('\x1b[35m', "Saved results to "+logfile);
-         console.log('\x1b[45m', "Saved results to "+logfile);
       });
       crawler.start();
 });
